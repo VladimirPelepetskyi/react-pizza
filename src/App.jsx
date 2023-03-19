0ebb969578af9df26e05a0ec817dc0f52e0c3c3f
VladimirPelepetskyi <vladimirpelepetskiy@gmail.com>
2023-03-19 12:04:46 +0200

    #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import pizzas from './assets/pizzas.json'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 import Categories from './Components/Categories/Categories'
 import Header from './Components/Header/Header'
 import PizzaBlock from './Components/PizzaBlock/PizzaBlock'
@@ -6,6 +7,14 @@ import Sort from './Components/Sort/Sort'
 import './scss/app.scss'
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    axios.get('https://6416d3ce47092b8b6135b890.mockapi.io/items').then((res) => {
+      setPizzas(res.data)
+    })
+  }, [])
+
   return (
     <div className='wrapper'>
       <Header />
