8e2967e63c033da28093f2a8345d5b6d5ddf3e30
VladimirPelepetskyi <vladimirpelepetskiy@gmail.com>
2023-03-21 14:07:26 +0200

    #10 Pagination

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,5 +1,20 @@
-const Pagination = () => {
-  return <div>Pagination</div>
+import ReactPaginate from 'react-paginate'
+import styles from './Pagination.module.scss'
+
+const Pagination = ({currentPage, onPageChanged, pageSize, totalPageCount}) => {
+  return (
+    <ReactPaginate
+      className={styles.root}
+      breakLabel='...'
+      previousLabel='<'
+      nextLabel='>'
+      onPageChange={(evt) => onPageChanged(evt.selected + 1)}
+      initialPage={currentPage - 1}
+      pageRangeDisplayed={pageSize}
+      pageCount={totalPageCount}
+      disabledClassName={'hidePrevNext'}
+    />
+  )
 }
 
 export default Pagination
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import axios from 'axios'
 import {useEffect, useState} from 'react'
 import Categories from '../Components/Categories/Categories'
+import Pagination from '../Components/Pagination/Pagination'
 import PizzaBlock from '../Components/PizzaBlock/PizzaBlock'
 import SkeletonPizzaBlock from '../Components/PizzaBlock/SkeletonPizzaBlock'
 import Sort from '../Components/Sort/Sort'
-import Pagination from '../Components/Pagination/Pagination'
 
 const Home = ({searchValue}) => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [categoryId, setCategoryId] = useState(0)
   const [sortType, setSortType] = useState({name: 'популярности  ↑', sortProperty: 'rating'})
+  const [currentPage, setCurrentPage] = useState(1)
+
+  //Кастыль. MockApi не дает общеее количество items
+  const totalItemsCount = 10
+  const pageSize = 4
+  const totalPageCount = Math.ceil(totalItemsCount / pageSize)
 
   useEffect(() => {
     setIsLoading(true)
@@ -21,15 +27,15 @@ const Home = ({searchValue}) => {
 
     axios
       .get(
-        `https://6416d3ce47092b8b6135b890.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
+        `https://6416d3ce47092b8b6135b890.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}&page=${currentPage}&limit=${pageSize}`
       )
       .then((res) => {
         setItems(res.data)
       })
       .catch(() => console.warn(`Some problems with mockAPI, please reload the page`))
       .finally(() => setIsLoading(false))
-    window.scrollTo(0, 0)
-  }, [sortType, categoryId, searchValue])
+    // window.scrollTo(0, 0)
+  }, [sortType, categoryId, searchValue, currentPage])
 
   const skeletons = [...new Array(6)].map((_, index) => <SkeletonPizzaBlock key={index} />)
   const pizzas = items
@@ -44,7 +50,12 @@ const Home = ({searchValue}) => {
       </div>
       <h2 className='content__title'>Все пиццы</h2>
       <div className='content__items'>{isLoading ? skeletons : pizzas}</div>
-      <Pagination />
+      <Pagination
+        currentPage={currentPage}
+        onPageChanged={setCurrentPage}
+        pageSize={pageSize}
+        totalPageCount={totalPageCount}
+      />
     </div>
   )
 }
